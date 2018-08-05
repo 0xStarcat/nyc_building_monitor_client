9830e9895752cf234fd862bce93eddd668f2acf3
Jade <jeffahking@gmail.com>
2018-08-05 03:31:38 -0400

    copy

--- src/Pages/ContentPage/Content/SupportContent.js
+++ src/Pages/ContentPage/Content/SupportContent.js
@@ -3,7 +3,11 @@ import React from 'react'
 import './style.scss'
 
 const SupportContent = () => {
-  return <div className="text-content">Support this page.</div>
+  return (
+    <div className="text-content">
+      <p>Support this page. (coming soon!)</p>
+    </div>
+  )
 }
 
 export default SupportContent
