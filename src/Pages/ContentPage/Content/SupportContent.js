42de6032d29e33479af011f8312f85df6a822aa5
Jade <jeffahking@gmail.com>
2018-08-05 21:18:46 -0400

    add survey link

--- src/Pages/ContentPage/Content/SupportContent.js
+++ src/Pages/ContentPage/Content/SupportContent.js
@@ -6,6 +6,12 @@ const SupportContent = () => {
   return (
     <div className="text-content">
       <p>Support this page. (coming soon!)</p>
+      <p>
+        <a href="https://goo.gl/forms/oajXFoqCKPXGy0Sn1" target="_blank" rel="noopener noreferrer">
+          Please take a short user experience survey
+        </a>{' '}
+        to help improve the site!
+      </p>
     </div>
   )
 }
