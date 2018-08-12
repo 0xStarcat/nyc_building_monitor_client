bb5355098409c2bfc9c025e04ed17577179a5316
0xStarcat <xxStarcat@gmail.com>
2018-08-11 21:50:37 -0400

    support

--- src/Pages/ContentPage/Content/SupportContent.js
+++ src/Pages/ContentPage/Content/SupportContent.js
@@ -15,7 +15,7 @@ const SupportContent = () => {
 
       <p>
         The entire project is built using my free labor and time. If you are in a position to do so, please consider{' '}
-        <a href="ko-fi.com/jadexx" target="_blank" rel="noopener noreferrer">
+        <a href="https://ko-fi.com/jadexx" target="_blank" rel="noopener noreferrer">
           donating a few dollars
         </a>{' '}
         to support this work and the server costs for maintaining this project.
