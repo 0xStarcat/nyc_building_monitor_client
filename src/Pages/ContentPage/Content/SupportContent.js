28468e57939a20f6d553c9815ece58386df7f940
0xStarcat <xxStarcat@gmail.com>
2018-08-11 21:45:51 -0400

    support

--- src/Pages/ContentPage/Content/SupportContent.js
+++ src/Pages/ContentPage/Content/SupportContent.js
@@ -12,6 +12,14 @@ const SupportContent = () => {
         </a>{' '}
         to help improve the site!
       </p>
+
+      <p>
+        The entire project is built using my free labor and time. If you are in a position to do so, please consider
+        <a href="https://ko-fi.com/P5P6H7P5" target="_blank" rel="noopener noreferrer">
+          donating a few dollars
+        </a>{' '}
+        to support this work and the server costs for maintaining this project.
+      </p>
     </div>
   )
 }
